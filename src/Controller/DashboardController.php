2b0622eab88e2a40b9714b6685a23a935a4acc99
Yannie <yanniealanunay116@gmail.com>
2025-11-11 22:36:39 +0800

    thisworkstoo

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,18 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 final class DashboardController extends AbstractController
 {
-    
+
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
-    
-        return $this->render('dashboard/index.html.twig', [
-            'user' => $this->getUser(),
-        ]);
-    
+        if($this->isGranted('ROLE_ADMIN') || $this->isGranted('ROLE_USER')){
+
+            return $this->render('dashboard/index.html.twig');
+        } 
+         throw new AccessDeniedException('You do not have access to this page.');
     }
 }
