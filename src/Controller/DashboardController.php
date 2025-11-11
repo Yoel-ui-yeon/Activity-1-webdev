e4a16e0a94b3f6efcda11c614c6600919570c9f6
Yannie <yanniealanunay116@gmail.com>
2025-11-11 21:03:52 +0800

    fixing

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,13 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-class DashboardController extends AbstractController
+
+final class DashboardController extends AbstractController
 {
+    
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
-        return new Response("Welcome to the dashboard! (Protected Area)");
+    
+        return $this->render('dashboard/index.html.twig', [
+            'user' => $this->getUser(),
+        ]);
+    
     }
 }
